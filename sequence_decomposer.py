014ac967a07c6a04543696b21ecafc1286918eb0
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-05-01 11:24:07 +0530

    Improved boiler plate
    Also fixed documentation for decomposer.

--- sequence_decomposer.py
+++ sequence_decomposer.py
@@ -3,7 +3,7 @@ from cube import *
 def decompose_sequence(move_sequence):
     """
     Decomposes a move sequence like "R U R' U R U U R" to something that can be
-    executed by our hardware.
+    executed by our hardware. Returns decomposed sequence as a string.
     """
 
     moves = move_sequence.split(' ')
@@ -11,7 +11,11 @@ def decompose_sequence(move_sequence):
     decomposed_sequence = ''
 
     for move in moves:
-        if move == "U" or move == "U'":
-            pass
-        else if move == "D" or move == "D'":
-            pass
+        current_move = move
+        if move in ["U", "U'", "D", "D'"]:
+            if move == "U" or move == "U'":
+                pass
+            else if move == "D" or move == "D'":
+                pass
+
+        decompose_sequence += current_move + ' '
