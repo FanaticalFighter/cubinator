8b71aa5d328892e49ff98b227d1f360f7eb595c0
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-05-21 02:46:32 +0530

    Added much better optimization for z rotation

--- sequence_decomposer.py
+++ sequence_decomposer.py
@@ -14,7 +14,6 @@ def decompose_sequence(move_sequence):
         else:
             decomposed_sequence += move + ' '
 
-    print decomposed_sequence
     moves = decomposed_sequence.split(' ')
 
     decomposed_sequence = ''
@@ -36,7 +35,7 @@ def decompose_sequence(move_sequence):
 
     print decomposed_sequence
 
-    ds = decomposed_sequence.split(' ')
+    ds = decomposed_sequence[:-1].split(' ')  # remove the trailing whitespace
     new_ds = ''
     skipping = False
 
@@ -50,9 +49,46 @@ def decompose_sequence(move_sequence):
         else:
             new_ds += ds[i - 1] + ' '
 
-    decomposed_sequence = new_ds
+    decomposed_sequence = new_ds[:-1]  # removing the trailing whitespace
+
+    print decomposed_sequence
+
+    lastTurn = ''
+    ds = decomposed_sequence.split(' ')
+
+    ds.remove('')
+    i = 0
+    while i < len(ds):
+        print ds
+        if (ds[i] == 'z' or ds[i] == "z'" and lastTurn == ''):
+            lastTurn = ds[i]
+        elif (i < len(ds) - 1 and ((lastTurn == "z" and ds[i] == "z'") or (lastTurn == "z'" and ds[i] == "z")) and ds[i + 1][0] not in "RLzxy"):
+            ds[i + 1], ds[i] = ds[i], ds[i + 1]  # swap 'z' with next move
+        elif (ds[i] == lastTurn + "'" or ds[i] + "'" == lastTurn):
+            lastTurn = ''
+
+        print ds
+
+        new_ds = ''
+        for j in range(1, len(ds) + 1):
+            if j < len(ds) and (ds[j - 1] == ds[j] + "'" or ds[j - 1] + "''" == ds[j]):
+                skipping = True
+                continue
+            elif skipping:
+                skipping = False
+                continue
+            else:
+                new_ds += ds[j - 1] + ' '
+
+        ds = new_ds[:-1].split(' ')
+
+        i += 1
+
+
+
+    decomposed_sequence = ' '.join(ds)
 
     print decomposed_sequence
     return decomposed_sequence
 
-decompose_sequence("R U R' U R U2 R'")
+decompose_sequence("R U2 F U2")
